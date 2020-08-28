449f007f938f6e5f1d00dcc5695f807735719369
Backerich <25672920+Backerich@users.noreply.github.com>
2020-08-28 13:49:07 +0200

    extended

--- process_darwin_test.go
+++ process_darwin_test.go
@@ -3,9 +3,27 @@
 package ps
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestDarwinProcess_impl(t *testing.T) {
 	var _ Process = new(DarwinProcess)
+}
+
+func TestListProcesses(t *testing.T) {
+	p, err := Processes()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if len(p) <= 0 {
+		t.Fatal("should have processes")
+	}
+
+	for _, p1 := range p {
+		fmt.Println(p1.Executable())
+	}
+
+	fmt.Println("Number of processes running: ", len(p))
 }
